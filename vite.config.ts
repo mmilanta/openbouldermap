40069fad35717c3d2ee8ff920e5cf8b5c3a60ae9
mmilanta <marco.milanta@gmail.com>
2026-07-18 17:40:43 +0200

    Setup GitHub Pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/openbouldermap/',
   server: {
     port: 5173,
     // serve the tiles/ and data/ folders so the browser can fetch the PMTiles archive
